42fa19cce3cd718cf967553f82f53b5d685d42c8
zhaopiaoyang <zpypiao@gmail.com>
2023-10-10 10:57:13 +0800

    refector the strusture

--- numpy/demo.py
+++ numpy/demo.py
@@ -1,8 +1,21 @@
 # This is a python program which is used by learning
 # written by zhaopiaoyang
 
-import numpy as np
-
 arr = np.array([1, 2, 3, 4])
 
-print(arr[::2])
+# command copy is a deep copy
+x = arr.copy()
+
+# command view is a light copy
+y = arr.view()
+
+# change the original array
+arr[0] = 5
+
+# reshape is a view, and the array will be flatted if use the command "arr.reshape(-1)"
+z = arr.reshape(2, 2)
+
+
+print(arr)  
+print(arr.dtype, arr.shape)
+print(x, x.base, y, y.base, sep='\n')
--- school-assignment/first/one.py
+++ school-assignment/first/one.py
@@ -2,23 +2,3 @@
 # written by zhaopiaoyang
 
 import numpy as np
-
-
-arr = np.array([1, 2, 3, 4])
-
-# command copy is a deep copy
-x = arr.copy()
-
-# command view is a light copy
-y = arr.view()
-
-# change the original array
-arr[0] = 5
-
-# reshape is a view, and the array will be flatted if use the command "arr.reshape(-1)"
-z = arr.reshape(2, 2)
-
-
-print(arr)
-print(arr.dtype, arr.shape)
-print(x, x.base, y, y.base, sep='\n')
