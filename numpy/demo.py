70bc2818ac2f4d09802fba21c7cfb5b1c12bd020
zhaopiaoyang <zpypiao@gmail.com>
2023-12-11 16:31:06 +0800

    done

--- numpy/demo.py
+++ numpy/demo.py
@@ -1,6 +1,9 @@
 # This is a python program which is used by learning
 # written by zhaopiaoyang
 
+import numpy as np
+import pandas as pd
+
 arr = np.array([1, 2, 3, 4])
 
 # command copy is a deep copy
@@ -15,7 +18,8 @@ arr[0] = 5
 # reshape is a view, and the array will be flatted if use the command "arr.reshape(-1)"
 z = arr.reshape(2, 2)
 
+url = 'https://github.com/zpypiao/Advaced-Applied-Statistics/blob/main/data/data_one.xlsx'
 
-print(arr)
-print(arr.dtype, arr.shape)
-print(x, x.base, y, y.base, sep='\n')
+data = pd.read_excel(url, sheet_name='sheet1')
+
+print(data.head(10))
